a377538d4afe7a7e22c6508786c630fa1600edad
VanshPardhan001 <palv60838@gmail.com>
2026-03-22 21:43:33 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+while True:
+    print("\nSimple Calculator")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("Type 'exit' to quit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == 'exit':
+        print("Program exited")
+        break
+
+    if choice in ['1', '2', '3', '4']:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print("Result:", num1 + num2)
+
+        elif choice == '2':
+            print("Result:", num1 - num2)
+
+        elif choice == '3':
+            print("Result:", num1 * num2)
+
+        elif choice == '4':
+            if num2 != 0:
+                print("Result:", num1 / num2)
+         …
